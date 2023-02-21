2de7b8a09ff9ee7b0ebe147de3c3661e394852cb
EslamElshahawi <eslamelshahawi2004@gmail.com>
2023-02-21 06:10:53 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
